dd6303e0ce535f7a07d9628cffb5709b409c5b62
Arvind Naidu <arvind@thebility.engineer>
2020-08-27 22:45:19 +0800

    feat(blog): add twitter for gatsby
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
+        `gatsby-plugin-twitter`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
