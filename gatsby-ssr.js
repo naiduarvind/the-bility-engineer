bc6b4b6d4a7abd72343ebc4da8ebd7bbb171b050
Arvind Naidu <arvind@thebility.engineer>
2020-10-24 13:39:19 +0800

    feat(blog): add plausible tracking script
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,7 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <script data-domain="thebility.engineeer" src="https://plausible.io/js/plausible.js" type="text/javascript" async defer/>
     ]);
 
     setPostBodyComponents([
